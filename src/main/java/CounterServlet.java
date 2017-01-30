27424d312ab3c132d4a36384775c23edae444cf6
abdou <laajali.abdelmoughit@gmail.com>
2017-01-30 14:05:12 -0600

    completed exercise

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+
+/**
+ *
+ */
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int counter=0;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        counter+=1;
+        out.println("<h1>Count: " + counter +"</h1>");
+
+    }
+}
