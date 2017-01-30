a8064de395a93f007798d2e89447e63d4d8c87a7
abdou <laajali.abdelmoughit@gmail.com>
2017-01-30 12:33:19 -0600

    completed exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,27 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
 
+
+
+
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    private int counter=0;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        counter+=1;
+        String name = request.getParameter("name");
+        out.println("<h1>Count: " + counter +"</h1>");
+        if (name==null){
+            out.println("<h1>Hello, World!</h1>");
+        }else{
+            out.println("<h1>Hello, " + name +"</h1>");
+        }
     }
 }
