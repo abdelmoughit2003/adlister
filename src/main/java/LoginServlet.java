08058f694c5725bb04f47d8dc91e64c6eba71e53
abdou <laajali.abdelmoughit@gmail.com>
2017-01-31 13:19:04 -0600

    completed exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,21 +3,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login" )
+@WebServlet(name = "LoginServlet", urlPatterns = {"/"} )
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/login.jsp").forward(request, response);
-
-
-        if (request.getSession().getAttribute("user") == null){
-            response.sendRedirect("/profile");
-            return;
-        }
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
-
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -26,7 +17,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
-            response.sendRedirect("/profile.jsp");
+            response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
